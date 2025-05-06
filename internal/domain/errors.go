18cd8d2d30736c1cc47a90f83c27098a0d77e211
Isac-SS <isacsants679@gmail.com>
2025-05-06 15:19:51 -0300

    fix(errors) corrige nome da  variável

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,7 +3,7 @@ package domain
 import "errors"
 
 var (
-	ErrAccountFound      = errors.New("Account not found")
+	ErrAccountNotFound   = errors.New("Account not found")
 	ErrDuplicatedAPIKey  = errors.New("Api key already exists")
 	ErrInvoiceNotFound   = errors.New("Invoice not found")
 	ErrUnauthorizedAcess = errors.New("Unauthorized acess")
